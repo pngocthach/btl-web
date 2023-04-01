ba390458c48867d0ef35fe0f7e135235ac690828
jelly <jellypnt1311@gmail.com>
2023-04-01 22:46:42 +0700

    Init project

---
+++ server/server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+//test
+app.get("/api", (req, res) => {
+  res.json({
+    test: [1, 2, 3],
+  });
+});
+
+app.listen(5000, () => {
+  console.log("server started on port 5000");
+});
