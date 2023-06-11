94c7bb03210c9eb263c2594d0e2f60b292286ed5
awesome0112 <nguyenminhquan01122003@gmail.com>
2023-06-11 17:44:09 +0700

    cần thêm cookie

--- server/app.js
+++ server/app.js
@@ -15,23 +15,9 @@ const cors = require("cors");
 const setAssociations = require("./model/associations")();
 
 app.use(morgan("dev"));
-
+app.use(cors());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-
-//CORS error handling
-app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
-  );
-  if (req.method === "OPTIONS") {
-    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET");
-    return res.status(200).json({});
-  }
-  next();
-});
 
 //Routes
 app.use("/cars", carRoutes);
